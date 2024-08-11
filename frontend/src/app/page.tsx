f5cae832d0a97ed8f237c26559bf9ecb815b7bf8
Oussama_Bouchnaif <113904749+OussamaBouchnaif@users.noreply.github.com>
2024-08-11 16:38:15 +0200

    build a navBar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,23 +1,35 @@
 "use client";
-require('dotenv').config();
 
-import { useState, } from "react";
+import { useState } from "react";
+import { ChakraProvider, Grid, GridItem, Show } from "@chakra-ui/react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { ExpensesList } from "./project/components/ExpensesList";
-import ProductForm from "./About/ProductForm";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { NavBar } from "./components/NavBar";
 
-
-  function App() {
-
-    console.log('API URL:', process.env.NEXT_PUBLIC_BACK_URL);
-    return (
-      <>
-      <ProductForm></ProductForm>
-      </>
-      
-    );
-  }
-  
- 
+function App() {
+  console.log("API URL:", process.env.NEXT_PUBLIC_BACK_URL);
+  return (
+    <ChakraProvider>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+            <NavBar></NavBar>
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            ASIDE
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="blue">
+          MAIN
+        </GridItem>
+      </Grid>
+    </ChakraProvider>
+  );
+}
 
 export default App;
